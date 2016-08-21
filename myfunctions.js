c92c9942057d26111350579655c4ceac498ff81a
Logan McAnsh <l.m@loganmcansh.com>
2016-08-21 12:10:18 -0400

    v1.1, prepend the icon instead of append
    Signed-off-by: Logan McAnsh <l.m@loganmcansh.com>

--- myfunctions.js
+++ myfunctions.js
@@ -1,1 +1,1 @@
-$('head').append('<link rel="mask-icon" href="https://mcansh.github.io/Dapulse-Safari-Pinned-Tab-Icon/dapulse.svg" color="#000">');
+$('head').prepend('<link rel="mask-icon" href="https://mcansh.github.io/Dapulse-Safari-Pinned-Tab-Icon/dapulse.svg" color="#000">');
